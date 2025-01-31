fc3151a24e2ed63843c4cb495094afaaa2fb7b1b
AabirChowdhary <aabir.chowdhary@outlook.com>
2025-01-31 17:32:07 +0000

    bug fixes

--- main.ts
+++ main.ts
@@ -15,8 +15,206 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSpr
     info.changeScoreBy(1)
     sprites.destroy(otherSprite)
 })
+function startlevel () {
+    roblox = sprites.create(img`
+        . . . f f f f f f f f . . . . . 
+        . . . f f 6 6 6 6 f f . . . . . 
+        . . f f 6 6 6 6 6 6 f . . . . . 
+        . . f f 6 f 6 6 f 6 f b b . . . 
+        . . f 6 6 6 6 6 6 6 6 f b b . . 
+        . . . 6 6 6 6 6 6 f 6 6 6 b b . 
+        . . . 6 6 f f f f 6 6 . 6 6 b . 
+        . . . 6 6 6 6 6 6 6 6 . . 6 6 . 
+        . . . 6 6 6 6 6 6 6 6 . . . . . 
+        . . . d d d . . d d d . . . . . 
+        . . . d d d . . d d d . . . . . 
+        . . . d d d . . d d d . . . . . 
+        . . . d d d . . d d d . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(roblox, 100, 0)
+    if (current_level == 0) {
+        tiles.setCurrentTilemap(tilemap`level6`)
+    } else if (current_level == 1) {
+        tiles.setCurrentTilemap(tilemap`level0`)
+    } else {
+        game.gameOver(true)
+        game.setGameOverEffect(true, effects.confetti)
+    }
+    roblox.ay = 350
+    scene.cameraFollowSprite(roblox)
+    for (let value of list) {
+    	
+    }
+    for (let value of tiles.getTilesByType(assets.tile`myTile8`)) {
+        tiles.setTileAt(value, assets.tile`transparency16`)
+    }
+    tiles.placeOnRandomTile(roblox, assets.tile`myTile8`)
+    for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
+        coin = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 5 5 5 5 5 5 5 . . . . . 
+            . . . 5 4 4 4 4 4 4 4 5 . . . . 
+            . . 5 4 4 4 4 4 4 4 4 4 5 . . . 
+            . 5 4 4 4 4 5 5 5 4 4 4 4 5 . . 
+            . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
+            . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
+            . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
+            . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
+            . 5 4 4 4 4 4 4 4 4 4 4 4 5 . . 
+            . 5 4 4 4 5 5 5 5 5 4 4 4 5 . . 
+            . . 5 4 4 4 4 4 4 4 4 4 5 . . . 
+            . . . 5 4 4 4 4 4 4 4 5 . . . . 
+            . . . . 5 5 5 5 5 5 5 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.coin)
+        animation.runImageAnimation(
+        coin,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . f f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . f 5 . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . f f f f f f f . . . . . 
+            . . . f 5 5 5 5 5 5 5 f . . . . 
+            . . f 5 5 4 4 4 4 4 5 5 f . . . 
+            . . f 5 5 5 5 5 5 5 5 5 f . . . 
+            . . f 5 4 5 5 5 5 5 5 5 f . . . 
+            . . f 5 4 5 5 5 5 5 5 5 f . . . 
+            . . f 5 4 5 5 5 5 5 5 5 f . . . 
+            . . f 5 4 5 5 5 5 5 5 5 f . . . 
+            . . f 5 5 5 4 4 5 5 5 5 f . . . 
+            . . . f 5 5 5 5 5 5 5 f . . . . 
+            . . . . f f f f f f f . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . f 5 5 5 5 5 f . . . . . 
+            . . . f 5 5 4 4 4 5 5 f . . . . 
+            . . . f 5 5 5 5 5 5 5 f . . . . 
+            . . . f 5 4 5 5 5 5 5 f . . . . 
+            . . . f 5 4 5 5 5 5 5 f . . . . 
+            . . . f 5 4 5 5 5 5 5 f . . . . 
+            . . . f 5 4 5 5 5 5 5 f . . . . 
+            . . . f 5 5 4 4 5 5 5 f . . . . 
+            . . . . f 5 5 5 5 5 f . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . f 5 5 4 5 f . . . . . 
+            . . . . . f 5 5 5 5 f . . . . . 
+            . . . . . f 5 4 5 5 f . . . . . 
+            . . . . . f 5 4 5 5 f . . . . . 
+            . . . . . f 5 4 5 5 f . . . . . 
+            . . . . . f 5 4 5 5 f . . . . . 
+            . . . . . f 5 5 5 5 f . . . . . 
+            . . . . . . f 5 5 f . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        100,
+        true
+        )
+        tiles.placeOnTile(coin, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
+            beehive = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 5 . . . . . . . . 
+                . . . . . . 5 5 5 . . . . . . . 
+                . . . . . 5 5 5 5 5 . . . . . . 
+                . . . . 5 5 5 5 5 5 5 . . . . . 
+                . . . . 5 5 5 5 5 5 5 . . . . . 
+                . . . 5 5 5 5 5 5 5 5 5 . . . . 
+                . . . 5 5 5 5 5 5 5 5 5 . . . . 
+                . . . 5 5 5 5 5 5 5 5 5 . . . . 
+                . . . 5 5 5 5 5 5 5 5 5 . . . . 
+                . . 5 5 5 5 4 4 4 5 5 5 5 . . . 
+                . . 5 5 5 5 4 . 4 5 5 5 5 . . . 
+                . . 5 5 5 4 . . . 4 5 5 5 . . . 
+                . . 5 5 5 4 . . . 4 5 5 5 . . . 
+                . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+                `, SpriteKind.beehive)
+            tiles.placeOnTile(beehive, value)
+            music.play(music.stringPlayable("F G A G E F G E ", 120), music.PlaybackMode.LoopingInBackground)
+            tiles.setTileAt(value, assets.tile`transparency16`)
+            music.play(music.stringPlayable("G E C A F E D F ", 120), music.PlaybackMode.LoopingInBackground)
+            info.setLife(3)
+        }
+    }
+    current_level = 0
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     current_level += 1
+    startlevel()
+})
+info.onLifeZero(function () {
+    game.setGameOverEffect(false, effects.dissolve)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.beehive, function (sprite, otherSprite) {
     sprites.destroy(otherSprite)
@@ -106,6 +304,7 @@ let bee: Sprite = null
 let beehive: Sprite = null
 let coin: Sprite = null
 let roblox: Sprite = null
+let current_level = 0
 scene.setBackgroundColor(9)
 scene.setBackgroundImage(img`
     1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
@@ -229,195 +428,8 @@ scene.setBackgroundImage(img`
     6666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666
     6666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666
     `)
-let current_level = 0
-roblox = sprites.create(img`
-    . . . f f f f f f f f . . . . . 
-    . . . f f 6 6 6 6 f f . . . . . 
-    . . f f 6 6 6 6 6 6 f . . . . . 
-    . . f f 6 f 6 6 f 6 f b b . . . 
-    . . f 6 6 6 6 6 6 6 6 f b b . . 
-    . . . 6 6 6 6 6 6 f 6 6 6 b b . 
-    . . . 6 6 f f f f 6 6 . 6 6 b . 
-    . . . 6 6 6 6 6 6 6 6 . . 6 6 . 
-    . . . 6 6 6 6 6 6 6 6 . . . . . 
-    . . . d d d . . d d d . . . . . 
-    . . . d d d . . d d d . . . . . 
-    . . . d d d . . d d d . . . . . 
-    . . . d d d . . d d d . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(roblox, 100, 0)
-if (current_level == 0) {
-    tiles.setCurrentTilemap(tilemap`level6`)
-} else {
-    tiles.setCurrentTilemap(tilemap`level0`)
-}
-roblox.ay = 350
-scene.cameraFollowSprite(roblox)
-for (let value of list) {
-	
-}
-for (let value of tiles.getTilesByType(assets.tile`myTile8`)) {
-    tiles.setTileAt(value, assets.tile`transparency16`)
-}
-tiles.placeOnRandomTile(roblox, assets.tile`myTile8`)
-for (let value of tiles.getTilesByType(assets.tile`myTile6`)) {
-    coin = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . 5 4 4 4 4 4 4 4 5 . . . . 
-        . . 5 4 4 4 4 4 4 4 4 4 5 . . . 
-        . 5 4 4 4 4 5 5 5 4 4 4 4 5 . . 
-        . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
-        . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
-        . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
-        . 5 4 5 4 4 4 4 4 4 4 4 4 5 . . 
-        . 5 4 4 4 4 4 4 4 4 4 4 4 5 . . 
-        . 5 4 4 4 5 5 5 5 5 4 4 4 5 . . 
-        . . 5 4 4 4 4 4 4 4 4 4 5 . . . 
-        . . . 5 4 4 4 4 4 4 4 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.coin)
-    animation.runImageAnimation(
-    coin,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . f f . . . . . . . 
-        . . . . . . . f f . . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . . f f . . . . . . . 
-        . . . . . . . f f . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . f 5 . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . f f f f f f f . . . . . 
-        . . . f 5 5 5 5 5 5 5 f . . . . 
-        . . f 5 5 4 4 4 4 4 5 5 f . . . 
-        . . f 5 5 5 5 5 5 5 5 5 f . . . 
-        . . f 5 4 5 5 5 5 5 5 5 f . . . 
-        . . f 5 4 5 5 5 5 5 5 5 f . . . 
-        . . f 5 4 5 5 5 5 5 5 5 f . . . 
-        . . f 5 4 5 5 5 5 5 5 5 f . . . 
-        . . f 5 5 5 4 4 5 5 5 5 f . . . 
-        . . . f 5 5 5 5 5 5 5 f . . . . 
-        . . . . f f f f f f f . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f f f . . . . . . 
-        . . . . f 5 5 5 5 5 f . . . . . 
-        . . . f 5 5 4 4 4 5 5 f . . . . 
-        . . . f 5 5 5 5 5 5 5 f . . . . 
-        . . . f 5 4 5 5 5 5 5 f . . . . 
-        . . . f 5 4 5 5 5 5 5 f . . . . 
-        . . . f 5 4 5 5 5 5 5 f . . . . 
-        . . . f 5 4 5 5 5 5 5 f . . . . 
-        . . . f 5 5 4 4 5 5 5 f . . . . 
-        . . . . f 5 5 5 5 5 f . . . . . 
-        . . . . . f f f f f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f f f f . . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . f 5 5 4 5 f . . . . . 
-        . . . . . f 5 5 5 5 f . . . . . 
-        . . . . . f 5 4 5 5 f . . . . . 
-        . . . . . f 5 4 5 5 f . . . . . 
-        . . . . . f 5 4 5 5 f . . . . . 
-        . . . . . f 5 4 5 5 f . . . . . 
-        . . . . . f 5 5 5 5 f . . . . . 
-        . . . . . . f 5 5 f . . . . . . 
-        . . . . . . f f f f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `],
-    100,
-    true
-    )
-    tiles.placeOnTile(coin, value)
-    tiles.setTileAt(value, assets.tile`transparency16`)
-    for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
-        beehive = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 5 . . . . . . . . 
-            . . . . . . 5 5 5 . . . . . . . 
-            . . . . . 5 5 5 5 5 . . . . . . 
-            . . . . 5 5 5 5 5 5 5 . . . . . 
-            . . . . 5 5 5 5 5 5 5 . . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . . 5 5 5 5 4 4 4 5 5 5 5 . . . 
-            . . 5 5 5 5 4 . 4 5 5 5 5 . . . 
-            . . 5 5 5 4 . . . 4 5 5 5 . . . 
-            . . 5 5 5 4 . . . 4 5 5 5 . . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            `, SpriteKind.beehive)
-        tiles.placeOnTile(beehive, value)
-        music.play(music.stringPlayable("F G A G E F G E ", 120), music.PlaybackMode.LoopingInBackground)
-        tiles.setTileAt(value, assets.tile`transparency16`)
-        music.play(music.stringPlayable("G E C A F E D F ", 120), music.PlaybackMode.LoopingInBackground)
-        info.setLife(3)
-    }
-}
+current_level = 1
+startlevel()
 game.onUpdate(function () {
     roblox.setImage(img`
         . . . f f f f f f f f . . . . . 
